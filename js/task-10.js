dcd9d9f3ec0a3b0634ec8a279003417421cb9cf7
oldstuden <atikin3002@gmail.com>
2023-06-20 08:25:19 +0200

    add task10

--- js/task-10.js
+++ js/task-10.js
@@ -6,36 +6,23 @@ function getRandomHexColor() {
 const items = [];
 const box = document.querySelector('#boxes');
 const controls = document.querySelector('#controls');
-console.dir(controls);
-controls.children[0].addEventListener('blur', onBuild);
-let numberBuild = controls.children[0].min;
-function onBuild(evt) {
-  numberBuild = evt.currentTarget.value;
-  console.log(evt.currentTarget.value);
-  return numberBuild;
-}
-// const creatEl = document.querySelector('[data-create]');
-// creatEl.addEventListener('click', onCreat);
-// console.dir(creatEl);
-let widthDiv = 30;
-for (let i = 0; i < numberBuild; i += 1) {
-  const divEl = document.createElement('div');
-  divEl.style.width = `${widthDiv}px`;
-  divEl.style.height = `${widthDiv}px`;
-  divEl.style.backgroundColor = getRandomHexColor();
-  items.push(divEl);
-  widthDiv += 10;
-}
-
-// console.log(items.join(''));
-//
-
-// const divEl = document.createElement('div');
-// divEl.style.width = '30px';
-// divEl.style.height = '30px';
-// divEl.style.backgroundColor = getRandomHexColor();
 const creatEl = document.querySelector('[data-create]');
+const destroyEl = document.querySelector('[data-destroy]');
+let numberBuild;
 creatEl.addEventListener('click', onCreat);
 function onCreat() {
+  numberBuild = controls.children[0].value;
+  let widthDiv = 30;
+  for (let i = 0; i < numberBuild; i += 1) {
+    const divEl = document.createElement('div');
+    divEl.style.width = `${widthDiv}px`;
+    divEl.style.height = `${widthDiv}px`;
+    divEl.style.backgroundColor = getRandomHexColor();
+    items.push(divEl);
+    widthDiv += 10;
+  }
   box.append(...items);
 }
+destroyEl.addEventListener('click', () => {
+  box.innerHTML = ''; // box.remove();
+});
